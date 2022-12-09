ac796c85692c6fa47593472c82a12b3c520c0611
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-09 18:48:31 +0300

    Programm 3

---
+++ 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+            int chislo = Convert.ToInt32(Console.ReadLine());
+            if (chislo % 7 == 0 && chislo % 23 == 0)
+            {
+                Console.WriteLine(chislo + " -> ДА");
+            }
+            else
+                Console.WriteLine(chislo + " -> нет");
+            Console.ReadLine();
+
