722fd436b0b2ce624cdb25a43d0cc7a840f237a1
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-09 18:44:26 +0300

    Programm 2

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи число: ");
+int tri = Convert.ToInt32(Console.ReadLine());
+string triPROVERKA = Convert.ToString(tri);
+if (triPROVERKA.Length > 2)
+{
+Console.WriteLine("третья цифра =  " + triPROVERKA[2]);
+}
+else
+{
+Console.WriteLine("третьей цифры нет");
+}
+Console.ReadLine();
