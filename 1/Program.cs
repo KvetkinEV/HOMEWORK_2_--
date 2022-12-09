fab08bdba77e1a457eaa4d869498c91b4bbb1c93
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-09 18:46:29 +0300

    Programm 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,29 +1,5 @@
-﻿int number = ReadInt("Введите трехзначное число: ");
-int amount = number.ToString().Length;
-
-if (amount < 3 || amount > 3)
-{
-    Console.WriteLine("Вы ввели не трехзначное число");
-}
-else
-{
-    Console.WriteLine(InCenter(number));
-}
-
-
-
-// ФУНКЦИИ------------------------------------------------------------------------------------------------------
-
-// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем. 
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция вывода цифры стоящей в середине трехзначногно числа.
-int InCenter(int a)
-{ 
-    int result = ((a / 10) % 10);
-    return result;
-}
+﻿Console.Write("Введи трёхзначное число: ");
+int dva = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(dva);
+Console.WriteLine("вторая цифра этого числа =  " + stringNumber[1]);
+Console.ReadLine();
